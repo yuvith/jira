cbc361d93c10c1d447f6f02575ca46c8075f8a47
root <root@ip-172-31-3-110.ap-south-1.compute.internal>
2019-09-17 09:31:13 +0000

    Removed unwanted recipes

--- recipes/configure-jira.rb
+++
@@ -1,7 +0,0 @@
-##Updating jira.home 	
-def write_file(file_name)
-    file = File.open('/apps/jira/server/atlassian-jira/WEB-INF/classes/jira-application.properties', 'w')
-    file.puts "jira.home = /apps/jira/server/jira_data"
-    file.close
-  end
-puts write_file("jira-application.properties") 
